b3c7675602a6bd1c3ced9fdc3ad3fa3c0d3116a0
cyberspacedk <frontend.dk@gmail.com>
2019-03-18 18:45:27 +0200

    cleaning odd files

--- react_redux_starter_kit/src/Redux/Reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers } from 'redux'; 
-import testInput from './testReducer';
-
-
-const rootReducer = combineReducers ({  
-    testValue: testInput,
-})
-
-export default rootReducer; 
